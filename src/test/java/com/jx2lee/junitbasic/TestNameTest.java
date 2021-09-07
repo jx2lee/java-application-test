1fed3b53343f759f64008d6c72ab7f6a415ebbc8
jx2lee <jaejun.lee.1991@gmail.com>
2021-09-07 20:29:32 +0900

    더 자바, 애플리케이션을 테스트하는 다양한 방법: JUnit 5 테스트 이름 표시하기(p.4)

---
+++ src/test/java/com/jx2lee/junitbasic/TestNameTest.java
@@ -0,0 +1,26 @@
+package com.jx2lee.junitbasic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class TestNameTest {
+
+    @Test
+    void under_score_remove() {
+        System.out.println("under_score 가 제거되는 것을 확인할 수 있습니다.");
+    }
+
+    @Test
+    void 함수명을_따라_테스트_이름이_설정됩니다() {
+        System.out.println("TestNameTest.함수명을_따라_테스트_이름이_설정됩니다");
+    }
+
+    @Test
+    @DisplayName("하지만_DisplayName_을_적용하면_우선순위가_높다")
+    void 하지만_DisplayName_을_적용하면_우선순위가_높다() {
+        System.out.println("TestNameTest.하지만_DisplayName_을_적용하면_우선순위가_높다");
+    }
+}
