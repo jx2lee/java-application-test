be9c91006d8454c46248f9f30393017a2c37e7f8
jx2lee <jaejun.lee.1991@gmail.com>
2021-10-19 15:40:09 +0900

    더 자바, 애플리케이션을 테스트하는 다양한 방법: JUnit 5: Junit4 마이그레이션 (p.16)
    * 실질적인 마이그레이션 작업은 없음
    * vintage engine 을 통해 junit4 테스트도 가능함 (junitvintage package)

---
+++ src/test/java/com/jx2lee/junitbasic/junitvintage/JunitVintageTest.java
@@ -0,0 +1,18 @@
+package com.jx2lee.junitbasic.junitvintage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitVintageTest {
+
+
+    @Before
+    public void Junit4_로_테스트하기_before() {
+        System.out.println("JunitVintageTest.Junit4_로_테스트하기_before");
+    }
+
+    @Test
+    public void Junit4_로_테스트하기() {
+        System.out.println("JunitVintageTest.Junit4_로_테스트하기");
+    }
+}
