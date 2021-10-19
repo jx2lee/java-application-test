0f0e56f6c9e6c80e0bb5a30e212e98466fbbf345
jx2lee <jaejun.lee.1991@gmail.com>
2021-10-19 11:54:22 +0900

    더 자바, 애플리케이션을 테스트하는 다양한 방법: JUnit 5: 테스트 순서 (p.12)

---
+++ src/test/java/com/jx2lee/junitbasic/testsequence/JunitTestSequenceTest.java
@@ -0,0 +1,50 @@
+package com.jx2lee.junitbasic.testsequence;
+
+import com.jx2lee.junitbasic.Class;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class JunitTestSequenceTest {
+
+    @Order(2)
+    @Test
+    @DisplayName("테스트_순서를_Order_annotation_을_이용해_설정한다_2")
+    void 테스트_순서를_Order_annotation_을_이용해_설정한다_2() {
+
+        System.out.println("JunitTestSequenceTest.테스트_순서를_Order_annotation_을_이용해_설정한다_2");
+        Class testClass = new Class(1);
+        assertThat(testClass.getLimit()).isGreaterThan(0);
+
+        System.out.println("testClass.getLimit() = " + testClass.getLimit());
+        System.out.println("==========");
+    }
+
+    @Order(1)
+    @Test
+    @DisplayName("테스트_순서를_Order_annotation_을_이용해_설정한다_1")
+    void 테스트_순서를_Order_annotation_을_이용해_설정한다_1() {
+
+        System.out.println("JunitTestSequenceTest.테스트_순서를_Order_annotation_을_이용해_설정한다_1");
+        Class testClass = new Class(2);
+        assertThat(testClass.getLimit()).isGreaterThan(1);
+
+        System.out.println("testClass.getLimit() = " + testClass.getLimit());
+        System.out.println("==========");
+    }
+
+    @Order(3)
+    @Test
+    @DisplayName("테스트_순서를_Order_annotation_을_이용해_설정한다_3")
+    void 테스트_순서를_Order_annotation_을_이용해_설정한다_3() {
+
+        System.out.println("JunitTestSequenceTest.테스트_순서를_Order_annotation_을_이용해_설정한다_3");
+        Class testClass = new Class(3);
+        assertThat(testClass.getLimit()).isGreaterThan(2);
+
+        System.out.println("testClass.getLimit() = " + testClass.getLimit());
+        System.out.println("==========");
+    }
+
+}
